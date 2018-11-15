c94249172fb4db09afc84a68f59b7becba3c7e6c
yumoz <43719395+yumoz@users.noreply.github.com>
2018-11-15 00:44:27 -0600

    Add files via upload

---
+++ 延时与清屏.cpp
@@ -0,0 +1,58 @@
+#include<stdlib.h>//«Â∆¡ 
+#include<stdio.h>  
+#include<windows.h> //—” ± 
+int main()
+{
+	int a,i;
+	a=1000;
+	for(i=0;i<5;i++)
+	{
+		
+	printf("π˛π˛\t");
+	
+	Sleep(a);
+	system("CLS");
+	printf(" «\n"); 
+	Sleep(a);
+//	system("PAUSE");
+	system("CLS");
+//	system("PAUSE");
+	printf("ø»ø»");
+	Sleep(a);
+	system("CLS") ;
+	}
+	
+	return 0;
+}
+
+//«Â∆¡∫Ø ˝
+/*
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	printf("hello world!\n");
+	printf("hello world!\n");
+	printf("hello world!\n");
+	system("PAUSE");
+	system("CLS");
+	system("PAUSE");
+	return 0;
+}
+/*
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	char c;
+	printf("asdhui");
+	printf("asdhui");
+	printf("asdhui");
+	printf("Y/N");
+	scanf("%d",&c);
+	if(c=='Y'||c=='y')
+	{
+		clrscr();
+	}
+	
+}*/
